bc213bd0cf4b9cb8ef3806f91dec5733d589452e
Yu Liang <wildmb23@gmail.com>
2014-12-04 11:31:48 -0800

    Remove bootstrap and install spin.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,7 +174,7 @@ module.exports = function (grunt) {
       options: {
         sourceMap: true,
         includePaths: ['bower_components']
-        },
+      },
       dist: {
         files: [{
           expand: true,
